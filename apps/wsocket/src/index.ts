6e2db78e056c598d239d1d48f23ba2037a4eb8c5
Pratham Sahu <prathamsahu051@gmail.com>
2024-11-27 23:02:37 +0530

    fix: changed port numer

--- apps/wsocket/src/index.ts
+++ apps/wsocket/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from "ws";
 import { UserManager } from "./UserManager";
 
-const wss = new WebSocketServer({ port: 3002 })
+const wss = new WebSocketServer({ port: 5000 })
 
 wss.on("connection", (ws) => {
   // Create a new User and connect it to a ws
