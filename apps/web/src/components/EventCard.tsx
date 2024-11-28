f808bacea1a513993534df744c4ec2a6f8441785
Pratham Sahu <prathamsahu051@gmail.com>
2024-11-28 23:47:18 +0530

    feat: created event card and nav

---
+++ apps/web/src/components/EventCard.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { FC } from "react";
+import { Card } from "./ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import { useSidebarStore } from "@/store/sidebar-store";
+
+interface EventCardProps {
+  description: string
+  imageUrl: string
+}
+
+
+
+const EventCard: FC<EventCardProps> = ({ description, imageUrl }) => {
+  const setOpen = useSidebarStore((state) => state.setOpen)
+
+  return (
+    <Card className="col-span-6 p-2 h-56 bg-white">
+      <div className="flex items-center text-xs">
+        <Image
+          src={
+            "https://probo.gumlet.io/image/upload/probo_product_images/Bar_Chart.png"
+          }
+          alt="adfa"
+          height={20}
+          width={20}
+        />
+        29880 traders
+      </div>
+
+      <div className="grid grid-cols-12">
+        <Image
+          src={imageUrl}
+          alt="adfa"
+          height={200}
+          width={100}
+          className="rounded-lg col-span-3"
+        />
+        <p className="col-span-9 font-medium text-lg">
+          {/* India to win the 3rd Test vs New Zealand? */}
+          {description}
+        </p>
+      </div>
+      <p className="text-xs mt-2">IND trail by 149 runs</p>
+
+      <div className="flex space-x-1 mt-3">
+        <Link
+          href={""}
+          className={cn(
+            buttonVariants({ variant: "yes" }),
+            "w-full font-semibold"
+          )}
+          onClick={() => setOpen(true)}
+        >
+          Yes ₹0
+        </Link>
+        <Link
+          href={""}
+          className={cn(
+            buttonVariants({ variant: "no" }),
+            "w-full font-semibold"
+          )}
+          onClick={() => setOpen(true)}
+        >
+          No ₹0
+        </Link>
+      </div>
+    </Card>
+  );
+};
+
+export default EventCard;
---
+++ apps/web/src/components/EventNav.tsx
@@ -0,0 +1,68 @@
+import { FC } from 'react'
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from './ui/navigation-menu'
+import Link from 'next/link'
+
+interface EventNavProps {
+  
+}
+
+const EventNav: FC<EventNavProps> = ({}) => {
+  return (
+    // By default Navigation menu has some z index so I have to remove z index that's why I have put z-0
+    <NavigationMenu className='container mx-auto max-w-[1370px] z-0 p-5'>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              All Events
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Cricket
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Crypto
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Football
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Stocks
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Chess
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              News
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+export default EventNav
